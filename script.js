2aed40168d4c6747b2b51dae6345467c5d2600cb
Arusi Yadav <yadavarushi04@gmail.com>
2024-04-16 17:00:50 +0530

    update today

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-
+let username='SRK'
 const userId=1024;
 const city = 'Noida';
 const state ='up';
+
+
+function getDetails(){
+    console.log(username)
+    console.log(userId)
+    console.log(city)
+    console.log(state)
+
+
+}
+getDetails()
+
+console.log("hshfiwejskj")
